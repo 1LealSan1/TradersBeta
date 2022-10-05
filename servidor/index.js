e3615218577242e6ebc2d6a67b0c3e4b39504862
Leonel <solis2e@outlook.com>
2022-10-04 23:49:51 -0600

    servidor hecho
    servidor hecho

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,15 @@
-console.log("Desde index.js")
+const express = require("express");
+
+//creamos el servidor
+const app = express();
+//asignamos puerto
+port=4000;
+
+//asignamos una ruta
+app.get("/",(req, res) =>{
+    res.send("hola a todos el servidor corre en el puerto " + port)
+})
+
+app.listen(port, () => {
+    console.log("el servidor corre en el puerto", port)
+})
